be25e8686f4f623cf78b12834d3d17de2d9a4060
Mustapha Sodiq Abubakar <minahfazz@gmail.com>
2023-07-13 13:37:23 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  * main - A Program that prints new line to the standard error
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	fput("and that piece of art is useful\"- Dora Korpar, 2015-10-19\r", stdout);
+	return (1);
+}
